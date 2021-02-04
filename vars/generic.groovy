97f397aaff350b323db66c0ebc5fce585e0a6b54
sungki96 <30778193+sungki96@users.noreply.github.com>
2021-02-04 16:27:11 +0530

    Update generic.groovy

--- vars/generic.groovy
+++ vars/generic.groovy
@@ -4,6 +4,7 @@ import groovy.transform.Field
 //import com.cloudbees.hudson.plugins.modeling.impl.auxiliary.AuxInstance
 import org.jenkinsci.plugins.workflow.support.steps.input.ApproverAction
 import org.jenkinsci.plugins.scriptsecurity.scripts.*
+import com.demo.common.ColorStep
 
 properties([pipelineTriggers([githubPush()])])
 
@@ -15,15 +16,16 @@ def pipelineDirectory = "";
 def tf_path = TF_PATH
 //def tf_path = ""
 def exec_node = (config.exec_node) ? config.exec_node : 'master'
+ColorStep color = new ColorStep()
     //agent any
     node(exec_node) {
         pipelineDirectory = "${env.WORKSPACE}/home/mgmtbld/demo_03"
         stage('GIT CHECKOUT') {
             
                 dir('/home/mgmtbld/demo_03') {
-                    println('SCM Checkout started')
+                    color.blue('SCM Checkout started')
                     checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'subhashree', url: 'https://github.com/hiteshcloudwork/platformui.git']]]
-                    println('SCM Checkout Completed')
+                    color.green('SCM Checkout Completed')
                     sh script: '''
                     ls -la
                     pwd
@@ -36,12 +38,12 @@ def exec_node = (config.exec_node) ? config.exec_node : 'master'
                   
             
         }
-        stage('INFRA CREATION') {
+        /*stage('INFRA CREATION') {
         
                 //tf_path = configurationYML.terraform.tfPath
                 println('TF_PATH'+tf_path)
             dir(tf_path) {
-                    println('TARGET INFRA CREATION STARTED')
+                    color.blue('TARGET INFRA CREATION STARTED')
                     sh script: ''' 
                         pwd
                         terraform fmt | sed -i '1d' main.tf
@@ -49,11 +51,36 @@ def exec_node = (config.exec_node) ? config.exec_node : 'master'
                         terraform plan
                         terraform apply -auto-approve
                     '''
-                    println('TARGET INFRA CREATED')
+                    color.green('TARGET INFRA CREATED')
                 }
             
-        }
-    
+        }*/
+		stage('VALIDATION') {
+			dir(tf_path) {
+			color.blue('VALIDATION STARTED')
+			def ipaddress = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_instance") | .values.network_interface[].network_ip'""", returnStdout: true).toString().trim()
+			def instance_name = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_instance") | .values.name'""", returnStdout: true).toString().trim()
+			def dns_name = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_dns_managed_zone") | .values.dns_name'""", returnStdout: true).toString().trim()
+			def dns_record = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_dns_record_set") | .values.name'""", returnStdout: true).toString().trim()
+			def network_name = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_network") | .values.name'""", returnStdout: true).toString().trim()
+			def subnetwork_name = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_subnetwork") | .values.name'""", returnStdout: true).toString().trim()
+			def serviceAccount = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_service_account") | .values.display_name'""", returnStdout: true).toString().trim()
+			def fireWall = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_firewall") | .values.name'""", returnStdout: true).toString().trim()
+			def imageName = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_image") | .values.family'""", returnStdout: true).toString().trim()
+			def diskName = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_disk") | .values.name'""", returnStdout: true).toString().trim()
+			def templateName = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_instance_template") | .name'""", returnStdout: true).toString().trim()
+			def sqlInstance = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_sql_database_instance") | .name'""", returnStdout: true).toString().trim()
+			def dbName = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_sql_database") | .values.name'""", returnStdout: true).toString().trim()
+			def sqlUser = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_sql_user") | .values.name'""", returnStdout: true).toString().trim()
+			def forwardingRule = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_forwarding_rule") | .values.name'""", returnStdout: true).toString().trim()
+			def backendService = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_region_backend_service") | .values.name'""", returnStdout: true).toString().trim()
+			def healthCheck = sh(script: """terraform show -json | jq -r '.values.root_modules.resource[] | select (.type=="google_compute_region_health_check") | .values.name'""", returnStdout: true).toString().trim()
+			color.blue("INSTANCE IP ADDRESS: ${ipaddress} \nINSTANCE NAME: ${instance_name} \nDNS NAME: ${dns_name} \DNS RECORD SET: ${dns_record} \nNETWORK NAME: ${network_name} \nSUBNETWORK NAME: ${subnetwork_name}
+			\nSERVICE ACCOUNT: ${serviceAccount} \nFIREWALL NAME: ${fireWall} \nIMAGE NAME: ${imageName} \nDISK NAME: ${diskName} \nTEMPLATE NAME: ${templateName} \nSQL INSTANCE: ${sqlInstance} 
+			\nDB NAME: ${dbName} \nSQL USER: ${sqlUser} \nFORWARDING RULE: ${forwardingRule} \nBACKEND SERVICE NAME: ${backendService} \nHEALTH CHECK NAME: ${healthCheck}")
+			color.green('VALIDATION COMPLETED')
+			}
+		}
     }
 }
 
